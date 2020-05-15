337a824064af04a3849eb7a1178bc3f8c0d15b63
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-05-14 17:46:48 -0700

    finished setting up server file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var PORT =process.env.PORT || 8080;
 
 var app =express();
 
+require("dotenv").config();
+
 app.use(express.static("public"));
 
 app.use(express.urlenconded({extended:true}));
@@ -16,6 +18,15 @@ var router =require("./controllers/app-apiroutes.js");
 
 app.use(router);
 
-app.listen(PORT, function(){
-    console.log("Server Listening on: http://localhost:" + PORT);
+//import models for syncing
+
+var db= require("./models");
+
+
+db.sequelize.sync().then(function(){
+
+    app.listen(PORT, function(){
+        console.log("Server Listening on: http://localhost:" + PORT);
+    });
+
 });
