d17d2792f0838f4ab64bfcfe473f031378ef6009
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-05-21 18:25:06 -0700

    Routes finished in need of testing

--- controllers/app-apiroutes.js
+++ controllers/app-apiroutes.js
@@ -1,6 +1,7 @@
 var express =require("express");
 var path =require("path");
 var axios =require("axios");
+var fs =require("fs");
 
 var router =express.Router();
 
@@ -34,12 +35,38 @@ router.get("/api/movies/:title", function(req,res){
     include:[db.MoviesUserVotes]
   }).then(function(results){
       if(results.length===0){
-    var query="http://www.omdbapi.com/?t="
+    var query="http://www.omdbapi.com/?t=&apikey=trilogy";
+    console.log(query);
+    axios.get(query).then(function(res){
+        console.log("****************The results we want*****************");
+        console.log("Movie Title:", res.data.Title);
+        console.log("Year Released:", res.data.Year);
+        console.log("Movie Genre:", res.data.Genre);
+        console.log("Movie Plot", res.data.Plot);
+        console.log("Movie Poster", res.data.Poster);
+        
+    })
+    .catch(function(err){
+        if(err.res){
+            console.log("-----------DATA-------------");
+            console.log(err.res.data);
+            console.log("--------------STATUS---------");
+            console.log(err.res.status);
+            console.log("-------------headers---------");
+            console.log(err.res.headers); 
+            
+        }else if (err.req){
+            console.log(err.req);
+            
+        }else{
+            console.log("Error", err.message);
+        }
+    })
 
       }else{
           res.json(results);
       }
-  })
+  });
    
 
 });
@@ -60,7 +87,7 @@ router.post("/api/movies/", function(req,res){
     res.sendStatus(200);
    }).catch(function(err){
        res.json(err);
-   })
+   });
     
 
 
@@ -69,16 +96,26 @@ router.post("/api/movies/", function(req,res){
 });
 
 //use this route to change a rate in movie
-router.put("/api/user/:vote", function(req,res){
+router.put("/api/user/:votes", function(req,res){
 //sequelize.update function
+db.MoviesUserVotes.update({
+    where:{votes:req.params.votes},
+    include:[db.movies,db.User]
+}).then(function(results){
+    res.json(results);
+})
 //users can vote on movies in their user pages and change the default value to 
 //whatever vote the user gives it
-//res.sendFile
+
 });
 
 // use this route to delete 
 router.delete("/api/user/movies/:id", function(req,res){
 //sequelize.destroy function{}
+db.Movies.destroy({
+    where:{id:req.params.id},
+    include:[db.MoviesUserVotes]
+});
 // so that we can delete the movie from the user page of user
 //QUESTION -how do i only delete from the userpage and not the database
 //possible answer: code in a way that targets only the association between user
